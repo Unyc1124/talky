6a105deb2d2830febc7595750ae6ea77eaf0d021
Unyc1124 <aditidubey792@gmail.com>
2024-09-11 13:52:01 +0530

    start of project

---
+++ lib/screens/auth.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class _AuthScreenState extends StatelessWidget {}
